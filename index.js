68ac2eda5d7e43d863aa9cafc26fe661e2f5997a
Frida Wieweg <fridaelisee@hotmail.com>
2023-06-13 15:44:16 +0200

    testar

--- index.js
+++ index.js
@@ -14,12 +14,14 @@
 // server.listen(port);
 
 const jsonServer = require('json-server');
+
 const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.db = router.db;
 server.use(auth);
 server.use(router); // Use '/api' as the base URL for your API
 
