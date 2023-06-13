b7268a3894da60170cfd48b1788bbef7c0b3841b
Frida Wieweg <fridaelisee@hotmail.com>
2023-06-13 14:57:12 +0200

    added cors

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+
 const port = process.env.PORT || 3001
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
